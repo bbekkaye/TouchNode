087cedcba2aede722d37ba01af24cdda7d6fc55e
Bachir Bekkaye <bachirbk@gmail.com>
2024-12-18 03:13:51 -0800

    Hello World node

---
+++ MyNodes/MyFirstNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TouchNode
+{
+    public class MyFirstNode
+    {
+        public MyFirstNode() { }
+        public static string HelloWorld()
+        {
+            return "Hello world";
+        }
+    }
+}
