04dc20e37f7fdcde84ac1691ef788b2f768f7dd5
Bachir Bekkaye <bachirbk@gmail.com>
2024-12-21 05:39:28 -0800

    Create new sheet view count node

---
+++ Revit/Views/Sheet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+
+namespace TouchNode.Revit.Views.Sheet
+{
+    /// <summary>
+    /// Wrapper class for the Sheet
+    /// </summary>
+    public class Sheet
+    {
+        private Sheet() { }
+
+        /// <summary>
+        /// Get all placed views on a sheet
+        /// </summary>
+        /// <param name="sheet"></param>
+        /// <returns></returns>
+        public static int GetAllPlacedViews(global::Revit.Elements.Views.Sheet sheet)
+        {
+            // Get the internal element
+            ViewSheet viewSheet = sheet.InternalElement as ViewSheet;
+
+            var placedViews = viewSheet.GetAllPlacedViews();
+
+            return placedViews.Count;
+        }
+    }
+}
